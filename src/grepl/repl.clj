868eb80f25df139ff5d64123572e88094f64d5ec
dan sutton <dan@dpsutton.com>
2021-04-11 15:42:43 -0500

    cleanup and add a function to invoke the repl

--- src/grepl/repl.clj
+++ src/grepl/repl.clj
@@ -33,20 +33,15 @@
   (def db (d/db conn))
   (def graph (d/graph db)))
 
-(def results-xf (map (fn [x] (try (edn/read-string x) (catch Throwable _ x)))))
-
-(defn root-of [form]
-  (let [db (d/db conn)]
-    (->> (d/q '[:find [?root-form ...]
-                :in $ ?form
-                :where
-                [?e :grepl/form ?form]
-                [?e :grepl/root ?root]
-                [?root :grepl/form ?root-form]]
-              db
-              (pr-str form))
-         (map (fn [x] (try (edn/read-string x) (catch Throwable _ x))))
-         (run! pprint))))
+(defn root-of [db form]
+  (d/q '[:find [?root-form ...]
+         :in $ ?form
+         :where
+         [?e :grepl/form ?form]
+         [?e :grepl/root ?root]
+         [?root :grepl/form ?root-form]]
+       db
+       (pr-str form)))
 
 (comment
   (let [form 'le
@@ -63,20 +58,28 @@
          (map (fn [x] (try (edn/read-string x) (catch Throwable _ x))))
          (run! pprint))))
 
-(defn parent-of [form]
-  (let [db (d/db conn)]
-    (->> (d/q '[:find [?parent-form ...]
-                :in $ ?form
-                :where
-                [?e :grepl/form ?form]
-                [?e :grepl/parent ?parent]
-                [?parent :grepl/form ?parent-form]]
-              db
-              (pr-str form))
-         (map (fn [x] (try (edn/read-string x) (catch Throwable _ x))))
-         (run! pprint))))
+(defn parent-of [db form]
+  (d/q '[:find [?parent-form ...]
+         :in $ ?form
+         :where
+         [?e :grepl/form ?form]
+         [?e :grepl/parent ?parent]
+         [?parent :grepl/form ?parent-form]]
+       db
+       (pr-str form)))
+
+(def results-xf (map (fn [x] (try (edn/read-string x) (catch Throwable _ x)))))
+
+(defn pprint-results
+  [results]
+  (binding [*print-meta* false]
+    (transduce results-xf
+               (completing (fn [_ r] (pprint r)))
+               nil
+               results)))
 
 (comment
+
   (require '[vlaaad.reveal :as reveal])
   (add-tap (reveal/ui))
 
@@ -85,10 +88,41 @@
             (if (and (sequential? form)
                      (keyword? (first form))
                      (#{"grepl"} (namespace (first form))))
-              (case (first form)
-                  :grepl/root (root-of (second form))
-                  :grepl/parent (parent-of (second form)))
+              (let [db (d/db conn)]
+                (case (first form)
+                  :grepl/root (pprint-results (root-of db (second form)))
+                  :grepl/parent (pprint-results (parent-of db (second form)))))
               (do (d/transact conn {:tx-data (->tx-data form)})
                   (eval form))))
-    ; :prompt (fn [] (printf "history aware> "))
+                                        ; :prompt (fn [] (printf "history aware> "))
+    :read clojure.core.server/repl-read))
+
+(defn grepl
+  "Start a clojure.main/repl which stores repl history in the graph
+  database asami. To search for the history, type `(:grepl/parent
+  <form>)` or `(:grepl/root <form>)`. There is no need to quote forms
+  to prevent evaluation.
+
+  Example:
+  grepl.repl=> (let [c 2 d 4] (+ c d))  ;; enter a form at the repl
+  6
+  grepl.repl=> (:grepl/parent d)        ;; query for the parents of d, of which there are two
+  [c 2 d 4]
+  (+ c d)
+  nil
+  grepl.repl=> (:grepl/root d)          ;; query for the root form, of which there is one
+  (let [c 2 d 4] (+ c d))
+  nil"
+  []
+  (clojure.main/repl
+    :eval (fn [form]
+            (if (and (sequential? form)
+                     (keyword? (first form))
+                     (#{"grepl"} (namespace (first form))))
+              (let [db (d/db conn)]
+                (case (first form)
+                  :grepl/root (pprint-results (root-of db (second form)))
+                  :grepl/parent (pprint-results (parent-of db (second form)))))
+              (do (d/transact conn {:tx-data (->tx-data form)})
+                  (eval form))))
     :read clojure.core.server/repl-read))
